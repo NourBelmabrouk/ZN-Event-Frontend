3b66e3f89b4ad717b7c5e20ddf14617476f37065
nour <nourbelmabrouk11@gmail.com>
2021-04-20 16:24:27 +0100

    authentication

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import './App.css';
 
 import React, {Component} from 'react';
 import Navbar from './components/navbar/Navbar';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
+
 
 class App extends Component {
   render() {
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Popup from "reactjs-popup";
-import  SignUp from  '../SignUp';
+import  SignUp from '../authentication_forms/SignUp';
+import  SignIn from '../authentication_forms/SignIn';
 import 'reactjs-popup/dist/index.css';
 
 const navbar=()=>{
 
 
     return(
+        <div>
         <Popup trigger={<button> Sign Up</button>} position="center center" modal nested>
             {
                 close =>(
@@ -21,6 +23,22 @@ const navbar=()=>{
                 )
             }
         </Popup>
+
+    <Popup trigger={<button> Sign In</button>} position="center center" modal nested>
+        {
+            close =>(
+                <div >
+                    <button onClick={close}>
+                        &times;
+                    </button>
+                    <div >
+                        <SignIn />
+                    </div>
+                </div>
+            )
+        }
+    </Popup>
+        </div>
     );
 };
 
