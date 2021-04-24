19f31d9dcee6eb6733e674ce3b894068ef6d4239
nour <nourbelmabrouk11@gmail.com>
2021-04-24 14:26:31 +0100

    minor fixe

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@
 import './App.css';
 
 import React, {Component} from 'react';
-import Navbar from './components/navbar/Navbar';
 
 
 class App extends Component {
   render() {
-    return (
-        <Navbar />
-    );
+
   }
 }
 
